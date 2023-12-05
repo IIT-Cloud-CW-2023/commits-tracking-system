be611cab339d6605ff920882eb3bf6b07a93be70
hiranjanperera7532 <hiranjanperera7532@gmail.com>
2023-12-05 08:06:14 +0530

    Initial Commit to the project

---
+++ src/main/java/com/DeveloperProductivityTracker/commitstrackingsystem/CommitsTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.DeveloperProductivityTracker.commitstrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitsTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitsTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DeveloperProductivityTracker/commitstrackingsystem/CommitsTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DeveloperProductivityTracker.commitstrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitsTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
